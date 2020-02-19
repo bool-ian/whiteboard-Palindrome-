d0523bcd14529c06140517c95a1a8804249c2c68
bool-ian <59983589+bool-ian@users.noreply.github.com>
2020-02-19 16:00:06 -0500

    Add project files.

---
+++ whiteboard/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace whiteboard
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, revString = "";
+            Console.WriteLine("Enter string: ");
+            str = Console.ReadLine();
+            str = str.ToLower();
+            for(int i=str.Length-1; i>=0; i--)
+            {
+                revString = revString + str[i];
+            }
+            Console.WriteLine($"Reverse String is {revString}");
+            if (str == revString)
+            {
+                Console.WriteLine("String is Palindrome");
+            }
+            else
+                Console.WriteLine("String is not palindrome");
+            Console.ReadLine();
+        }
+ 
+    }
+}
